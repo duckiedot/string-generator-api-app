a2c44c9b9f916c8976a06148da7adca32bc8e70f
duckiedot <m.malinkiewicz01@gmail.com>
2022-10-18 21:09:48 +0200

    Init commit

---
+++ src/main/java/com/string/generator/assignment/StringGeneratorApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.string.generator.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringGeneratorApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringGeneratorApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/string/generator/assignment/StringGeneratorApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.string.generator.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringGeneratorApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
